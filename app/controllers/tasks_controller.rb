97cd4c8e079cba455a99776d665c5d8aec9a5ff6
Grant Hall <grantclone@gmail.com>
2023-02-14 16:33:44 +0900

    all crud commands for tasks completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to show_path(@task)
+    redirect_to task_path(@task)
   end
 
   # update
@@ -25,10 +25,18 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # def update
-  #   @task =
-  # end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   # destroy
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
   get '/tasks', to: 'tasks#index', as: :tasks
-  get '/tasks/new', to: 'tasks#new', as: :new
-  get '/tasks/:id', to: 'tasks#show', as: :show
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
   post '/tasks', to: 'tasks#create'
   get '/task/:id', to: 'tasks#show'
-  get '/tasks/:id/edit', to: 'tasks#edit'
-  patch '/tasks', to: 'tasks#update'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
